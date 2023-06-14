ec66434af010cd46203a456fb402a70777c11d50
Steve Basse <84519539+iSteveB@users.noreply.github.com>
2023-06-14 20:41:55 +0200

    Hero section and navbar ended

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,9 +1,36 @@
-import React from 'react';
+"use client";
+
+import Image from 'next/image';
+import { CustomButton } from '@/components';
 
 const Hero = () => {
+    const handleScroll = () => {
+
+    }
     return (
-        <div>
-            
+        <div className='hero'>
+            <div className='flex-1 pt-36 padding-x'>
+                <h1 className='hero__title'>
+                    Find, Book or Rent a car -- Quickly and Easily!
+                </h1>
+
+                <p className='hero__subtitle'>
+                    Streamline your car rental experience with effortless
+                    booking process.
+                </p>
+
+                <CustomButton
+                    title='Explore Cars'
+                    containerStyles='bg-primary-blue text-white rounded-full mt-10'
+                    handleClick={handleScroll}
+                 />
+            </div>
+            <div className="hero__image-container">
+                <div className="hero__image">
+                    <Image src='/hero.png' alt='hero' fill className='object-contain'/>
+                </div>
+                < div className='hero__image-overlay' />
+            </div>
         </div>
     );
 };
